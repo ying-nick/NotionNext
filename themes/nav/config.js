6af825d6c47a336262f8c7cecaca9bc46fc660bf
DM <77763075+ying-nick@users.noreply.github.com>
2025-04-21 22:03:59 +0800

    Update config.js

--- themes/nav/config.js
+++ themes/nav/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  NAV_INDEX_PAGE: 'about', // 文档首页显示的文章，请确此路径包含在您的notion数据库中
+  NAV_INDEX_PAGE: 'archive', // 文档首页显示的文章，请确此路径包含在您的notion数据库中
 
   NAV_AUTO_SORT: process.env.NEXT_PUBLIC_NAV_AUTO_SORT || true, // 是否自动按分类名 归组排序文章；自动归组可能会打乱您Notion中的文章顺序
 
